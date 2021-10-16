1550345e1a921b25ae5ccdce7ae96efdc5db5bf8
asam-1337 <samenkov0280@gmail.com>
2021-10-16 14:42:52 +0300

    Inittial commit

---
+++ cmake-build-debug/CMakeFiles/lab3_2.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chicha-man/CLionProjects/lab3_2/PCB_s_2.cpp" "/home/chicha-man/CLionProjects/lab3_2/cmake-build-debug/CMakeFiles/lab3_2.dir/PCB_s_2.cpp.o"
+  "/home/chicha-man/CLionProjects/lab3_2/main.cpp" "/home/chicha-man/CLionProjects/lab3_2/cmake-build-debug/CMakeFiles/lab3_2.dir/main.cpp.o"
+  "/home/chicha-man/CLionProjects/lab3_2/menu.cpp" "/home/chicha-man/CLionProjects/lab3_2/cmake-build-debug/CMakeFiles/lab3_2.dir/menu.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab3_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab3_2.dir/PCB_s_2.cpp.o"
+  "CMakeFiles/lab3_2.dir/main.cpp.o"
+  "CMakeFiles/lab3_2.dir/menu.cpp.o"
+  "lab3_2"
+  "lab3_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab3_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
